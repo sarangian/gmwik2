0a97d82ba24aec476733fe1510b7664f2e4d3b51
Aditya Narayan Sarangi <a.n.sarangi@gmail.com>
2024-09-09 15:09:56 +0530

    Update pipeline.py

--- src/GMWI2/pipeline.py
+++ src/GMWI2/pipeline.py
@@ -45,168 +45,6 @@ def rm_r(path):
   elif os.path.exists(path):
       os.remove(path)
 
-def check_GRCh38():
-  database_dir = os.path.join(utils.DEFAULT_DB_FOLDER, "GRCh38_noalt_as")
-  hash_dir = os.path.join(utils.DEFAULT_DB_FOLDER, "GRCh38_md5sum.txt")
-  if not os.path.exists(database_dir):
-    return False
-
-  cur = os.getcwd()
-  os.chdir(database_dir)
-
-  proc = subprocess.Popen(
-      ["md5sum", "-c", hash_dir], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-  )
-  os.chdir(cur)
-  proc.communicate()
-  return proc.returncode == 0
-
-def dependency_checks():
-  print(
-        "-" * 11,
-        "Dependency checks",
-        "-" * 11,
-  )
-
-  # -----------------------check metaphlan-------------------------------------
-  spinner = Halo(text="MetaPhlAn v3.0.13", spinner=spin)
-  spinner.start()
-
-  try:
-      proc = subprocess.Popen(
-          ["metaphlan", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-      )
-      output = proc.stdout.read().decode("ASCII")
-      correct = "MetaPhlAn" in output
-      correct_version = "3.0.13" in output
-  except:
-      correct = False
-
-  if not correct or not correct_version:
-    spinner.fail()
-    if correct:
-          printw(output.split("\n")[0])
-          printw("Incorrect version of MetaPhlAn")
-    else:
-          printw("MetaPhlAn not found on path")
-    printw('Please run: "mamba install -c bioconda metaphlan=3.0.13"')
-    printr("GMWI2 aborted " + poop)
-    sys.exit()
-  else:
-    spinner.succeed()
-  # -----------------------check metaphlan-------------------------------------
-
-
-
-  # -----------------------check other dependencies----------------------------
-  dependencies = ["repair.sh", "fastqc", "bowtie2", "samtools", "bedtools", "trimmomatic"]
-  for d in dependencies:
-    spinner = Halo(text=d, spinner=spin)
-    spinner.start()
-
-    try:
-        proc = subprocess.Popen(
-            [d, "--version" if d != "trimmomatic" else "-version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        output = proc.stdout.read().decode("ASCII")
-        proc.communicate()
-        correct = proc.returncode == 0
-    except:
-        correct = False
-
-    if not correct:
-      spinner.fail()
-      printw(f"{d} not found on path")
-      install = d if d != "repair.sh" else "bbmap"
-      printw(f'Please run: "mamba install -c bioconda {install}"')
-      printr("GMWI2 aborted " + poop)
-      sys.exit()
-    else:
-      spinner.succeed()
-
-  print(
-        "-" * 41,
-        "\n"
-  )
-
-def database_installation():
-  print(
-        "-" * 9,
-        "Database installation",
-        "-" * 9,
-  )
-  spinner = Halo(text="Installing MetaPhlAn marker database", spinner=spin)
-  spinner.start()
-
-  command = [
-    "metaphlan",
-    "--install",
-    "--index", 
-    "mpa_v30_CHOCOPhlAn_201901",
-  ]
-  proc = subprocess.Popen(command, stderr=subprocess.PIPE)
-
-  stderr = proc.stderr.read().decode("utf-8") 
-
-  proc.communicate()
-  incorrect = proc.returncode != 0
-
-  if "MD5 checksums do not correspond!" in stderr or incorrect:
-    spinner.fail()
-    printw(stderr)
-    printr("GMWI2 aborted " + poop)
-    sys.exit()
-  else:
-    spinner.succeed()
-
-
-  spinner = Halo(text="Downloading GRCh38/hg38", spinner=spin)
-  spinner.start()
-
-  if not check_GRCh38():
-    target_dir = os.path.join(utils.DEFAULT_DB_FOLDER)
-    zip_location = os.path.join(target_dir, "GRCh38_noalt_as.zip")
-
-    rm_r(os.path.join(target_dir, "GRCh38_noalt_as"))
-    rm_r(zip_location)
-
-    url = "https://genome-idx.s3.amazonaws.com/bt/GRCh38_noalt_as.zip"
-    proc = subprocess.Popen(f"wget -q -P {target_dir} {url}", shell=True, stderr=subprocess.PIPE)
-
-    stderr = proc.stderr.read().decode("utf-8") 
-
-    proc.communicate()
-    incorrect = proc.returncode != 0
-
-    if incorrect:
-      spinner.fail()
-      printw(stderr)
-      printr("GMWI2 aborted " + poop)
-      sys.exit()
-
-    zip_location = os.path.join(target_dir, "GRCh38_noalt_as.zip")
-
-    error = None
-    try:
-      with zipfile.ZipFile(zip_location, 'r') as zip_ref:
-        zip_ref.extractall(target_dir)
-    except Exception as e:
-       error = traceback.format_exc()
-
-    if error:
-      spinner.fail()
-      printw(error)
-      printr("GMWI2 aborted " + poop)
-      sys.exit()
-
-    rm_r(zip_location)
-
-  spinner.succeed()
-  print(
-        "-" * 41,
-        "\n"
-  )
-
 def copy_input(args):
   spinner = Halo(text="Extracting/copying fastq files", spinner=spin)
   spinner.start()
@@ -331,7 +169,7 @@ def overrepresented(args):
   if proc.returncode != 0:
     spinner.fail()
     printw(stderr)
-    printr("GMWI2 aborted " + poop)
+    printr("GMWI2 aborted ")
     sys.exit()
 
   spinner.succeed()
@@ -357,45 +195,10 @@ def open_shell(command, spinner):
   if proc.returncode != 0:
     spinner.fail()
     printw(stderr)
-    printr("GMWI2 aborted " + poop)
+    printr("GMWI2 aborted ")
     sys.exit()
 
-def human(args):
-  spinner = Halo(text="Removing human reads", spinner=spin)
-  spinner.start()
 
-  # input: repaired1.fastq, repaired2.fastq
-  # output: mapped.sam
-  command = f"bowtie2 -p {args.num_threads} -x {os.path.join(utils.DEFAULT_DB_FOLDER, 'GRCh38_noalt_as/GRCh38_noalt_as')} -1 {args.output_prefix}_repaired1.fastq -2 {args.output_prefix}_repaired2.fastq -S {args.output_prefix}_mapped.sam"
-  open_shell(command, spinner)
-  rm_r(f"{args.output_prefix}_repaired1.fastq")
-  rm_r(f"{args.output_prefix}_repaired2.fastq")
-
-  # input: mapped.sam
-  # output: mapped.bam
-  command = f"samtools view -bS {args.output_prefix}_mapped.sam > {args.output_prefix}_mapped.bam"
-  open_shell(command, spinner)
-  rm_r(f"{args.output_prefix}_mapped.sam")
-
-  # input: mapped.bam
-  # output: human.bam
-  command = f"samtools view -b -f 12 -F 256 {args.output_prefix}_mapped.bam > {args.output_prefix}_human.bam"
-  open_shell(command, spinner)
-  rm_r(f"{args.output_prefix}_mapped.bam")
-
-  # input: human.bam
-  # output: human_sorted.bam
-  command = f"samtools sort -n {args.output_prefix}_human.bam -o {args.output_prefix}_human_sorted.bam -@ {args.num_threads}"
-  open_shell(command, spinner)
-  rm_r(f"{args.output_prefix}_human.bam")
-
-  # input: human_sorted.bam
-  # output: human1.fastq, human2.fastq
-  command = f"bedtools bamtofastq -i {args.output_prefix}_human_sorted.bam -fq {args.output_prefix}_human1.fastq -fq2 {args.output_prefix}_human2.fastq"
-  open_shell(command, spinner)
-  rm_r(f"{args.output_prefix}_human_sorted.bam")
-
-  spinner.succeed()
 
 def trim(args):
   spinner = Halo(text="Removing adapters and low quality reads", spinner=spin)
@@ -410,12 +213,12 @@ def trim(args):
 
   # input: adapters.txt, human1.fastq, human2.fastq
   # output: QC_xx.fastq.gz, 
-  command = f"trimmomatic PE -threads {args.num_threads} {args.output_prefix}_human1.fastq {args.output_prefix}_human2.fastq "
+  command = f"trimmomatic PE -threads {args.num_threads} {args.output_prefix}_repaired1.fastq {args.output_prefix}_repaired2.fastq "
   command += f"-baseout {args.output_prefix}_QC.fastq.gz ILLUMINACLIP:{args.output_prefix}_adapters.txt:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 MINLEN:60"
   open_shell(command, spinner)
   rm_r(f"{args.output_prefix}_adapters.txt")
-  rm_r(f"{args.output_prefix}_human1.fastq")
-  rm_r(f"{args.output_prefix}_human2.fastq")
+  rm_r(f"{args.output_prefix}_repaired1.fastq")
+  rm_r(f"{args.output_prefix}_repaired2.fastq")
 
   spinner.succeed()
 
@@ -432,7 +235,6 @@ def quality_control(args):
   copy_input(args)
   repair_reads(args)
   overrepresented(args)
-  human(args)
   trim(args)
 
   print(
